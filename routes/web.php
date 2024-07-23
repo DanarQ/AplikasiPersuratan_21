a0fcbdd353d3f908a97902dec2143dcd03d2cf43
Danar Qusyairi <danar4213@gmail.com>
2024-07-23 18:16:15 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
+    Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'registerPost'])->name('register');
+    Route::get('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
+    Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'loginPost'])->name('login');
+});
+
+  Auth::routes();
 Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
 Route::resource('/kepalasurat',\App\Http\Controllers\KepalaSuratController::class);
