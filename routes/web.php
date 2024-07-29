c60ea2a8328cf4c247b33a2eec265fe537ed0f39
Danar Qusyairi <danar4213@gmail.com>
2024-07-29 12:46:03 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ Route::group(['middleware' => 'guest'], function () {
     Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'loginPost'])->name('login');
 });
 
+Route::middleware('auth', 'user-access:admin')->name('admin.')->group(function () {
+    Route::resource('/admin/User', \App\Http\Controllers\UserController::class);
+    Route::resource('/admin/Kepala_surat', \App\Http\Controllers\BarangController::class);
+    Route::resource('/admin/Surat_keluar', \App\Http\Controllers\CustomerController::class);
+    Route::resource('/admin/Surat_masuk', \App\Http\Controllers\KeluhanController::class);
+    Route::get('/admin', [\App\Http\Controllers\HomeController::class, 'index'])->name('admin');
+    Route::delete('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+  });
   Auth::routes();
 Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
 Route::resource('/kepalasurat',\App\Http\Controllers\KepalaSuratController::class);
